410e84e8e54d66300d0d83e37de601455e104f7c
battleangel217 <idaraobong05@gmail.com>
2025-11-29 16:12:16 +0100

    Gemini api key

--- vote/consumers.py
+++ vote/consumers.py
@@ -7,8 +7,9 @@ from channels.db import database_sync_to_async
 from django.db.models import F
 from google import generativeai as genai
 from django.conf import settings
+import os
 
-genai.configure(api_key=settings.GEMINI_API_KEY)
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
 class VotingConsumer(AsyncWebsocketConsumer):
     # Class-level dict to track timers per room
