894ad00289a3f35165e31a2fe8980f39ab73eae4
battleangel217 <idaraobong05@gmail.com>
2025-12-06 12:50:43 +0100

    hosted storage

--- backend/Games/settings.py
+++ backend/Games/settings.py
@@ -69,7 +69,7 @@ INSTALLED_APPS = [
     'uvicorn',
     'vote',
     'health',
-    'aichat'
+    'aichat',
 ]
 
 MIDDLEWARE = [
@@ -111,25 +111,24 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": ['redis://127.0.0.1:6379'],
+            "hosts": [REDIS_URL],
         }
     },
 }
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
